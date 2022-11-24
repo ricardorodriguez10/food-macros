7b9f70ba36e23e9cec727bc183c87b138bfb40c4
Miquel de Domingo <miquel.dedomingo@xing.com>
2022-11-24 10:22:57 +0100

    chore: prettier, eslint & other minor tweaks

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,41 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
-  mode: "development",
-  devtool: "source-map",
+  mode: 'development',
+  devtool: 'source-map',
   /** @type {import('webpack-dev-server').Configuration} */
   devServer: {
-    allowedHosts: "all",
+    allowedHosts: 'all',
     static: {
-      directory: path.join(__dirname, "public"),
+      directory: path.join(__dirname, 'public'),
     },
     hot: true,
     compress: true,
     port: 8080,
-    client: { logging: "verbose", overlay: true, progress: true },
+    client: { logging: 'verbose', overlay: true, progress: true },
   },
   module: {
     rules: [
       {
         test: /\.js?$/,
         exclude: [/node_modules/],
-        use: { loader: "babel-loader" },
+        use: { loader: 'babel-loader' },
       },
       {
         test: /\.css?$/,
         exclude: [/node_modules/],
-        use: { loader: "style-loader" },
+        use: { loader: 'style-loader' },
       },
     ],
   },
   resolve: {
-    extensions: [".js"],
+    extensions: ['.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
+      template: path.join(__dirname, 'public', 'index.html'),
     }),
   ],
 };
